b2e3a7652e4a85e4312f6a80208590ef06d2ad61
r-baggioII <167486553+r-baggioII@users.noreply.github.com>
2024-08-27 10:59:41 -0300

    cambio caracter representación enemigos

--- src/Enemigos/Humano.java
+++ src/Enemigos/Humano.java
@@ -3,7 +3,7 @@ package Enemigos;
 public class Humano extends Enemigo{
 
     public Humano(int posX, int posY){
-        super( 100, 2, 5, 4,'±',posX,posY);
+        super( 100, 2, 5, 4,'H',posX,posY);
     }
 
     public void aumentarNivel(){}
