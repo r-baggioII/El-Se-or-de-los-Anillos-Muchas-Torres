818f1ed768aa44a84b7f72f62533f9bb0203e238
r-baggioII <167486553+r-baggioII@users.noreply.github.com>
2024-08-27 16:13:54 -0300

    Create clase Magia.java

---
+++ src/Magia.java
@@ -0,0 +1,35 @@
+public class Magia {
+    public final int magiaInicial = 100;
+    public final int magiaMaxima = 600;
+    public int magiaActual;
+    
+    //Constructor 
+    public Magia(){
+        this.magiaActual = magiaInicial;
+    }
+    //Getters
+    public int getMagiaActual(){
+        return magiaActual;
+    }
+    public int getMagiaInicial(){
+        return magiaInicial;
+    }
+    public int getMagiaMaxima(){
+        return magiaMaxima;
+    }
+    //Metodos 
+    public void aumentarMagia(int cantidad){
+        if(magiaActual + cantidad > magiaMaxima){
+            magiaActual = magiaMaxima;
+        }else{
+            magiaActual += cantidad;
+        }
+    }
+    public void gastarMagia(int cantidad){
+        if(magiaActual - cantidad < 0){
+            magiaActual = 0;
+        }else{
+            magiaActual -= cantidad;
+        }
+    }
+}
